43bff4c8c8885ad953ac7a4383d28937f32c3ced
adilraees786 <adilraees911@gmail.com>
2024-10-23 15:25:45 +0500

    homeset

--- src/screen/Homepage.jsx
+++ src/screen/Homepage.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function(){
     return(
-        <div>
+        <div className='flex justify-center'>
             <h1>Homepage</h1>
         </div>
     )
